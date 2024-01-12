04f08d14f48d8d41a9f8f1e8ea0508891b0ef67e
Nathan Gilbert <natedrewg@gmail.com>
2024-01-12 15:43:55 -0500

    Added Tailwind and some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { DaysCreateForm } from './ui-components';
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import awsExports from './aws-exports';
 Amplify.configure(awsconfig);
@@ -10,7 +9,19 @@ Amplify.configure(awsExports);
 function App() {
   return (
     <div className="App">
-      <DaysCreateForm />
+      <body>
+        <div class = "flow-root">
+          <div class = "float-center text-3xl bg-orange-50">
+            <h1><b>Welcome to my daily form!</b></h1>
+          </div>
+          <div class = "bg-orange-50 px-4 py-4 justify-center">
+            <DaysCreateForm />
+          </div>
+        </div>
+      </body>
+      
+
+      
     </div>
   );
 }
