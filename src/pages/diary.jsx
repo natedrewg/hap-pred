51a9fa28c093efe103cfb1eab4071c719d74d26f
Nathan Gilbert <natedrewg@gmail.com>
2024-02-15 22:09:55 -0500

    deleted attempt at query

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -1,10 +1,3 @@
-import { generateClient } from 'aws-amplify/api';
-import * as queries from '../graphql/queries';
-
-const client = generateClient();
-
-const allDays = await client.graphql({ query: queries.listDays });
-
 export const Diary = () => {
   return (
     <div class= "bg-ivory">
