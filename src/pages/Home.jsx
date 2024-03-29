2fdd3d91c2f93f6009fe81180cf5d35b3e6a1bca
Nathan Gilbert <natedrewg@gmail.com>
2024-03-28 21:29:06 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,20 @@ Amplify.configure(config);
 const client = generateClient();
 
 const result = await client.graphql({ query: listDays });
-
+const allDays = result.data.listDays.items
 const oneDay = await client.graphql({
   query: getDays ,
   variables: { id: 3 }
 });
+
+
 
 export const Home = () => {
   return (
     <div class= "bg-ivory">
       <body>
         <div>
-          {JSON.stringify(oneDay)}
+          {JSON.stringify(allDays)}
         </div>
       </body>
     </div>
