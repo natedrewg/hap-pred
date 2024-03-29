8572d1cc1c29911127767ad2fa7a474c6f48f5a9
Nathan Gilbert <natedrewg@gmail.com>
2024-03-28 21:00:58 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { listDays } from '../graphql/queries';
+import { listDays, getDays } from '../graphql/queries';
 import config from '../amplifyconfiguration.json';
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
@@ -9,8 +9,8 @@ const client = generateClient();
 const result = await client.graphql({ query: listDays });
 
 const oneDay = await client.graphql({
-  query: queries.getDay,
-  variables: { id: '3' }
+  query: getDays ,
+  variables: { id: 3 }
 });
 
 export const Home = () => {
