88aab39b3c4b3ebaf58af23b404d54c5161d8f8c
Nathan Gilbert <natedrewg@gmail.com>
2024-04-01 19:27:29 -0400

    Attempt at showing Specific days

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,43 @@ import { listDays, getDays } from '../graphql/queries';
 import config from '../amplifyconfiguration.json';
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
+import { Paper, Button} from '@mui/material'
+import React, { useState, useEffect } from 'react';
 Amplify.configure(config);
 
 const client = generateClient();
 
-const result = await client.graphql({ query: listDays });
-const allDays = result.data.listDays.items
-const oneDay = await client.graphql({
-  query: getDays ,
-  variables: { id: 3 }
-});
-
-
-
 export const Home = () => {
-  return (
+  const [Days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetchDays()
+  }, [])
+
+  const fetchDays = async () => {
+    try{
+      const result = await client.graphql({ query: listDays });
+      const allDays = result.data.listDays.items;
+      console.log('List of Days', allDays);
+      setDays(allDays)
+    } catch (error){
+      console.log('error on fetching days', error)
+    }
+  }
+return (
     <div class= "bg-ivory">
       <body>
-        <div>
-          {JSON.stringify(allDays)}
+        <div className="daysList">
+          { Days.map(Days => {
+            return(
+              <Paper variant="outlined" elevation={2} >
+                <div className="dayCard">
+                  <div className="dayId">{Days.id}</div>
+                </div>
+              </Paper>
+
+            )
+          })}
         </div>
       </body>
     </div>
