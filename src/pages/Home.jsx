7df37af9a2d6af67807c17060fbd1855f2094021
Nathan Gilbert <natedrewg@gmail.com>
2024-04-01 20:51:11 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,16 +38,16 @@ return (
                   <b><div className="dayId"><p>The day:</p>{Days.id}</div></b>
                   <br/>
                   
-                  <div className="dayHappy"><p>How happy I was out of 6 this day:</p>{Days.Happy}</div>
+                  <div className="dayHappy"><p>Happy:</p>{Days.Happy}</div>
                   <br/>
                   
-                  <div className="daySleep"><p>How much sleep I got the day before:</p>{Days.Sleep}</div>
+                  <div className="daySleep"><p>Sleep:</p>{Days.Sleep}</div>
                   <br/>
                   
-                  <div className="dayHealthy"><p>How healthy I felt at the end of the day:</p>{Days.Healthy}</div>
+                  <div className="dayHealthy"><p>Healthy:</p>{Days.Healthy}</div>
                   <br/>
                   
-                  <div className="dayMeals"><p>How many meals I had this day:</p>{Days.Meals}</div>
+                  <div className="dayMeals"><p>Meals:</p>{Days.Meals}</div>
                   <br/>
                   
                   <div className="dayDesc"><p>My short description of the day:</p>{Days.Description}</div>
