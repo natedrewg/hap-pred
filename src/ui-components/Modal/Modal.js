c12458689d1588c5c0d25be0b433674e6a62f03e
Nathan Gilbert <natedrewg@gmail.com>
2024-04-03 12:08:24 -0400

    Update Modal.js

--- src/ui-components/Modal/Modal.js
+++ src/ui-components/Modal/Modal.js
@@ -33,107 +33,107 @@ export default function Modal(Days) {
                 {Days.id}
               </div>
             </b>
-            <div className="day">
+            <div className="dayPractice">
               <p>Practice: </p>
               {Days.Practice}
             </div>
-            <div className="day">
+            <div className="dayIndo">
               <p>Individual Work: </p>
               {Days.Indo}
             </div>
-            <div className="day">
+            <div className="dayLift">
               <p>Lift: </p>
               {Days.Lift}
             </div>
-            <div className="day">
+            <div className="dayWallball">
               <p>Wallball: </p>
               {Days.WallBall}
             </div>
-            <div className="day">
+            <div className="dayPT">
               <p>PT: </p>
               {Days.PT}
             </div>
-            <div className="day">
+            <div className="dayGameday">
               <p>Gameday: </p>
               {Days.Game}
             </div>
-            <div className="day">
+            <div className="dayNap">
               <p>Nap: </p>
               {Days.Nap}
             </div>
-            <div className="day">
+            <div className="dayHomework">
               <p>Homework: </p>
               {Days.Homework}
             </div>
-            <div className="day">
-              <p>Videogame: </p>
+            <div className="dayVideoGame">
+              <p>Videogames: </p>
               {Days.VideoGames}
             </div>
-            <div className="day">
+            <div className="dayJessCall">
               <p>Call or hangout with Jess: </p>
               {Days.JessCall}
             </div>
-            <div className="day">
+            <div className="dayHangout">
               <p>Hangout with friends: </p>
               {Days.Hangout}
             </div>
-            <div className="day">
+            <div className="dayParents">
               <p>Call or hangout with parents: </p>
               {Days.Parents}
             </div>
-            <div className="day">
+            <div className="dayClasses">
               <p>Classes: </p>
               {Days.Classes}
             </div>
-            <div className="day">
+            <div className="dayPrayed">
               <p>Prayed: </p>
               {Days.Prayed}
             </div>
-            <div className="day">
+            <div className="dayBibleTime">
               <p>Read the Bible: </p>
               {Days.BibleTime}
             </div>
-            <div className="day">
+            <div className="dayBibleStudy">
               <p>Have Biblestudy: </p>
               {Days.BibleStudy}
             </div>
-            <div className="day">
+            <div className="daySatisfied">
               <p>Satisfied with the day: </p>
               {Days.Satisfied}
             </div>
-            <div className="day">
+            <div className="dayStressed">
               <p>Stressed out: </p>
               {Days.Stressed}
             </div>
-            <div className="day">
+            <div className="dayGoodEvent">
               <p>Good Event: </p>
               {Days.GoodEvent}
             </div>
-            <div className="day">
+            <div className="dayBadEvent">
               <p>Bad Event: </p>
               {Days.BadEvent}
             </div>
-            <div className="day">
+            <div className="dayMeals">
               <p>Amount of meals: </p>
               {Days.Meals}
             </div>
-            <div className="day">
+            <div className="dayStressedOut">
               <p>Stressed out level: </p>
               {Days.StressedOut}
             </div>
-            <div className="day">
+            <div className="dayHealthy">
               <p>How healthy: </p>
               {Days.Healthy}
             </div>
-            <div className="day">
+            <div className="daySleep">
               <p>How much sleep: </p>
               {Days.Sleep}
             </div>
-            <div className="day">
+            <div className="dayHappy">
               <p>How happy: </p>
               {Days.Happy}
             </div>
-            <div className="day">
+            <div className="dayDescription">
               <p>The Description of the day: </p>
               {Days.Description}
             </div>
