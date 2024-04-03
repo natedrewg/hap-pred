8e693b128b62ddd837043eed107628d819ce7ea8
Nathan Gilbert <natedrewg@gmail.com>
2024-04-02 23:53:29 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,8 @@ import config from '../amplifyconfiguration.json';
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 import { Paper } from '@mui/material'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../ui-components/Modal/Modal'
-
 Amplify.configure(config);
 
 const client = generateClient();
