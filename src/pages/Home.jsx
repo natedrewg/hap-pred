a341a47a194a891c37279b760c5bcb0d7baf2b0a
Nathan Gilbert <natedrewg@gmail.com>
2024-04-03 20:58:32 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,70 +9,11 @@ import Modal from "../ui-components/Modal/Modal";
 Amplify.configure(config);
 const client = generateClient();
 
+
 export const Home = () => {
-  const [days, setDays] = useState([]);
-
-  useEffect(() => {
-    fetchDays();
-  }, []);
-
-  const fetchDays = async () => {
-    try {
-      const result = await client.graphql({ query: listDays });
-      const allDays = result.data.listDays.items;
-
-      console.log("List of Days", allDays);
-      setDays(allDays || []);
-    } catch (error) {
-      console.log("error on fetching days", error);
-    }
-  };
-
-  const sortedDays = days.slice().sort((a, b) => {
-    return a.id - b.id;
-  });
-
   return (
     <div className="bg-ivory">
-      <body className="cards padding">
-        <div className="daysList">
-          {sortedDays.map((day) => {
-            return (
-              <Paper key={day.id} variant="outlined" elevation={2}>
-                <div className="dayCard">
-                  <b>
-                    <div className="dayId">
-                      <p>Day: {day.id}</p>
-                    </div>
-                  </b>
-
-                  <div className="dayHappy">
-                    <p>Happy:</p>
-                    {day.Happy}
-                  </div>
-
-                  <div className="daySleep">
-                    <p>Sleep:</p>
-                    {day.Sleep}
-                  </div>
-
-                  <div className="dayHealthy">
-                    <p>Healthy:</p>
-                    {day.Healthy}
-                  </div>
-
-                  <div className="dayMeals">
-                    <p>Meals:</p>
-                    {day.Meals}
-                  </div>
-
-                  <Modal days={day} />
-                </div>
-              </Paper>
-            );
-          })}
-        </div>
-      </body>
+      
     </div>
   );
 };
