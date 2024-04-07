48e1cae5da16da17e0368a89b065b418c5d5df64
Nathan Gilbert <natedrewg@gmail.com>
2024-04-07 13:00:25 -0400

    Added a count of how many days there are

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,35 @@ Amplify.configure(config);
 const client = generateClient();
 
 
+
 export const Home = () => {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetchDays();
+  }, []);
+
+  const fetchDays = async () => {
+    try {
+      const result = await client.graphql({ query: listDays });
+      const allDays = result.data.listDays.items;
+
+      console.log("List of Days", allDays);
+      setDays(allDays || []);
+    } catch (error) {
+      console.log("error on fetching days", error);
+    }
+  };
+
+  const countDays = () => {
+    return days.length;
+  };
+
+ 
+
   return (
     <div className="bg-ivory">
-      
+      <p>Number of Days: {countDays()}</p>
     </div>
   );
 };
