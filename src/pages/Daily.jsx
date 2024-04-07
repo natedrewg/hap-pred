19c68dee82bf0998d76dfdf9307a4ac895a1375c
Nathan Gilbert <natedrewg@gmail.com>
2024-04-07 14:33:14 -0400

    Update Daily.jsx

--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -1,6 +1,37 @@
 import { DaysCreateForm } from '../ui-components';
+import { listDays } from "../graphql/queries";
+import config from "../amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import React, { useState, useEffect } from "react";
+
+
+Amplify.configure(config);
+const client = generateClient();
 
 export const Daily = () => {
+  const [days, setDays] = useState([]);
+  
+  useEffect(() => {
+    fetchDays();
+  }, []);
+
+  const fetchDays = async () => {
+    try {
+      const result = await client.graphql({ query: listDays });
+      const allDays = result.data.listDays.items;
+
+      console.log("List of Days", allDays);
+      setDays(allDays || []);
+    } catch (error) {
+      console.log("error on fetching days", error);
+    }
+  };
+
+  const countDays = () => {
+    return days.length;
+  };
+
   return (
     <div className="App">
       <body class = "padding showHide">
@@ -9,7 +40,7 @@ export const Daily = () => {
             <h1>Welcome to my daily form!</h1>
           </div>
           <div class = "bg-sage justify-center pl-4 pr-4 flex">
-            <DaysCreateForm />
+            <DaysCreateForm id = {countDays()}/>
           </div>
         </div>
       </body>
