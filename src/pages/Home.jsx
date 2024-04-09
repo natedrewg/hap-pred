07d5e0f72e4373688beb1ded4c6e71df7d04f209
Nathan Gilbert <natedrewg@gmail.com>
2024-04-08 21:08:16 -0400

    left and right sections for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,41 +98,46 @@ export const Home = () => {
     <div className="bg-sage">
       <body className="padding">
         <div className="spacing"></div>
-      <Paper className="container bg-orange-50">
-          <h1 className="heading"><b>Introduction</b></h1>
-          <div className="data">
-            <p>This website tracks a senior lacrosse student athlete at Eastern University named Nathan Gilbert. Below you will be able to see insights about what he did everyday. The daily form tab is to add to the database. The diary tab is to show a summary of what everyday was like, as well as showing the entirety of the day with a description through a modal. </p>
-          
+        <div className="container">
+          <div className="left-section">
+            <Paper className="bg-orange-50">
+              <h1 className="heading"><b>Introduction</b></h1>
+              <div className="data">
+                <p>This website tracks a senior lacrosse student athlete at Eastern University named Nathan Gilbert. Below you will be able to see insights about what he did every day. The daily form tab is to add to the database. The diary tab is to show a summary of what everyday was like, as well as showing the entirety of the day with a description through a modal. </p>
+              </div>
+            </Paper>
+            <div className="spacer"></div>
+            <Paper>
+              <h1 className="heading"><b>Visualizations</b></h1>
+              <div className="data">
+                <h2>Averages: </h2>
+                <p>Number of Days: {countDays()}</p>
+                <p>Average Happiness: {calculateAverage('Happy')} / 6</p>
+                <p>Average Health: {calculateAverage('Healthy')} / 6</p>
+                <p>Average Sleep: {calculateAverage('Sleep')}</p>
+                <p>Average Meals: {calculateAverage('Meals')}</p>
+                <br/>
+                <h3>How many days I did certain things:</h3>
+                <p>Called or hungout with Jess: {calculateTotalTrue('JessCall')} / {countDays()}</p>
+                <p>Called or hungout with friends: {calculateTotalTrue('Hangout')} / {countDays()}</p>
+                <p>Called or hungout with my parents: {calculateTotalTrue('Parents')} / {countDays()}</p>
+                <p>Did homework: {calculateTotalTrue('Homewok')} / {countDays()}</p>
+                <p>Played video games: {calculateTotalTrue('VideoGames')} / {countDays()}</p>
+                <p>Took a nap: {calculateTotalTrue('Nap')} / {countDays()}</p>
+                <p>Had practice: {calculateTotalTrue('Practice')} / {countDays()}</p>
+                <p>Had a game: {calculateTotalTrue('Game')} / {countDays()}</p>
+              </div>
+            </Paper>
           </div>
-        </Paper>
-        <div className="spacer"></div>
-        <Paper className="container">
-          <h1 className="heading"><b>Visualizations</b></h1>
-          <div className="data">
-            <h2>Averages: </h2>
-            <p>Number of Days: {countDays()}</p>
-            <p>Average Happiness: {calculateAverage('Happy')} / 6</p>
-            <p>Average Health: {calculateAverage('Healthy')} / 6</p>
-            <p>Average Sleep: {calculateAverage('Sleep')}</p>
-            <p>Average Meals: {calculateAverage('Meals')}</p>
-            <br/>
-            <h3>How many days I did certain things:</h3>
-            <p>Called or hungout with Jess: {calculateTotalTrue('JessCall')} / {countDays()}</p>
-            <p>Called or hungout with friends: {calculateTotalTrue('Hangout')} / {countDays()}</p>
-            <p>Called or hungout with my parents: {calculateTotalTrue('Parents')} / {countDays()}</p>
-            <p>Did homework: {calculateTotalTrue('Homewok')} / {countDays()}</p>
-            <p>Played videogames: {calculateTotalTrue('VideoGames')} / {countDays()}</p>
-            <p>Took a nap: {calculateTotalTrue('Nap')} / {countDays()}</p>
-            <p>Had practice: {calculateTotalTrue('Practice')} / {countDays()}</p>
-            <p>Had a game: {calculateTotalTrue('Game')} / {countDays()}</p>
+          <div className="right-section">
+            <Paper className="container">
+              <h1 className="heading"><b>Graphs</b></h1>
+              <div className="data">
+                <canvas ref={canvasRef}></canvas>
+              </div>
+            </Paper>
           </div>
-        </Paper>
-        <Paper className="container">
-          <h1 className="heading"><b>Graphs</b></h1>
-          <div className="data">
-            <canvas ref={canvasRef}></canvas>
-          </div>
-        </Paper>
+        </div>
       </body>
     </div>
   );
