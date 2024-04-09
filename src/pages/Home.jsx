26e6aa3b5714b01b24c68f8c71f9e46e79133dc9
Nathan Gilbert <natedrewg@gmail.com>
2024-04-08 22:44:09 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -129,7 +129,7 @@ export const Home = () => {
         </Paper>
         
       </body>
-      <body>
+      <body className="noPadding">
         <Paper className="container">
           <h1 className="heading"><b>Graphs</b></h1>
           <div className="data">
