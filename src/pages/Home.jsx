2fb4d30c8e34056250b364876edfeb661011d438
Nathan Gilbert <natedrewg@gmail.com>
2024-04-09 21:29:19 -0400

    removed histogram

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ import GraphOne from "../ui-components/Graphs/GraphOne";
 import GraphTwo from "../ui-components/Graphs/GraphTwo";
 import GraphThree from "../ui-components/Graphs/GraphThree";
 import GraphFour from "../ui-components/Graphs/GraphFour";
-import GraphHisto from "../ui-components/Graphs/GraphHisto";
 
 import "./Home.css";
 
@@ -130,8 +129,6 @@ export const Home = () => {
         <GraphThree days={days} />
         <div className="spacerSmall"></div>
         <GraphFour days={days} />
-        <div className="spacerSmall"></div>
-        <GraphHisto days={days} />
       </body>
     </div>
   );
