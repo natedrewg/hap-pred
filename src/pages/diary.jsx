d75a4e676b73e1b1ec70a50b3367270b965f769b
Nathan Gilbert <natedrewg@gmail.com>
2024-04-10 22:51:59 -0400

    Update diary.jsx

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -57,7 +57,7 @@ export const Diary = () => {
               }}
             />
             <button
-              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mb-4"
+              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4"
               onClick={handleSearch}
             >
               Search
