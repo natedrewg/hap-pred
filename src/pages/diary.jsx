eb2e8f7fea74c95c5b3f8d8e76b062bcda0ba697
Nathan Gilbert <natedrewg@gmail.com>
2024-04-10 22:05:31 -0400

    Update diary.jsx

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -56,14 +56,14 @@ export const Diary = () => {
               }}
             />
             <button
-              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded pl-5"
               onClick={handleSearch}
             >
               Search
             </button>
           </Paper>
         </div>
-
+        <div className="spacerSmall"></div>
         <div className="daysList bg-orange-50">
           {(searchTerm ? searchResults : days).map((day) => (
             <Paper
