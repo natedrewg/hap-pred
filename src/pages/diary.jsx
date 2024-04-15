f9d52b4b57edc222eafc792ae7e436951b3223d7
Nathan Gilbert <natedrewg@gmail.com>
2024-04-15 12:37:50 -0400

    Update diary.jsx

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -13,7 +13,6 @@ export const Diary = () => {
   const [days, setDays] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [showSearchBar, setShowSearchBar] = useState(true); // State to manage search bar visibility
 
   useEffect(() => {
     fetchDays();
@@ -39,42 +38,32 @@ export const Diary = () => {
     setSearchResults(results);
   };
 
-  const handleFullDayButtonClick = () => {
-    setShowSearchBar(false); // Hide search bar when full day button is clicked
-  };
-
   return (
     <body>
-      <body
-        className={`padding showHide ${
-          showSearchBar ? "visible" : "invisible"
-        }`}
-      >
-        <div className="bg-sage flow-root">
+      <body className="padding showHide">
+        <div className="bg-sage flow-root invisible sm:invisible md:visible lg:visible xl:visible">
           <div className="search-bar">
-            {showSearchBar && ( // Render search bar based on showSearchBar state
-              <Paper className="p-3">
-                <TextField
-                  id="outlined-basic"
-                  className="w-full"
-                  label={searchTerm ? "" : "Search for a Day"}
-                  variant="outlined"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      handleSearch();
-                    }
-                  }}
-                />
-                <button
-                  className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4"
-                  onClick={handleSearch}
-                >
-                  Search
-                </button>
-              </Paper>
-            )}
+            <Paper className="p-3">
+              <TextField
+                id="outlined-basic"
+                className="w-full"
+                label={searchTerm ? "" : "Search for a Day"}
+                variant="outlined"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    handleSearch();
+                  }
+                }}
+              />
+              <button
+                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
+            </Paper>
           </div>
           <div className="spacerSmall"></div>
           <div className="daysList bg-orange-50">
@@ -91,24 +80,95 @@ export const Diary = () => {
                       <p>Day: {day.id}</p>
                     </div>
                   </b>
+
                   <div className="dayHappy">
                     <p>Happy:</p>
                     {day.Happy}
                   </div>
+
                   <div className="daySleep">
                     <p>Sleep:</p>
                     {day.Sleep}
                   </div>
+
                   <div className="dayHealthy">
                     <p>Healthy:</p>
                     {day.Healthy}
                   </div>
+
                   <div className="dayMeals">
                     <p>Meals:</p>
                     {day.Meals}
                   </div>
-                  <button onClick={handleFullDayButtonClick}>Full Day</button>{" "}
-                  {/* Add a button to hide search bar */}
+
+                  <Modal days={day} />
+                </div>
+              </Paper>
+            ))}
+          </div>
+        </div>
+      </body>
+      <body className="noPadding">
+        <div className="bg-sage flow-root visible sm:visible md:invisible lg:invisible xl:invisible">
+          <div className="search-bar">
+            <Paper className="p-3">
+              <TextField
+                id="outlined-basic"
+                className="w-full"
+                label="Search for a Day"
+                variant="outlined"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    handleSearch();
+                  }
+                }}
+              />
+              <button
+                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4"
+                onClick={handleSearch}
+              >
+                Search
+              </button>
+            </Paper>
+          </div>
+          <div className="spacerSmall"></div>
+          <div className="daysList bg-orange-50">
+            {(searchTerm ? searchResults : days).map((day) => (
+              <Paper
+                key={day.id}
+                variant="outlined"
+                elevation={2}
+                className="bg-orange-50"
+              >
+                <div className="dayCard">
+                  <b>
+                    <div className="dayId">
+                      <p>Day: {day.id}</p>
+                    </div>
+                  </b>
+
+                  <div className="dayHappy">
+                    <p>Happy:</p>
+                    {day.Happy}
+                  </div>
+
+                  <div className="daySleep">
+                    <p>Sleep:</p>
+                    {day.Sleep}
+                  </div>
+
+                  <div className="dayHealthy">
+                    <p>Healthy:</p>
+                    {day.Healthy}
+                  </div>
+
+                  <div className="dayMeals">
+                    <p>Meals:</p>
+                    {day.Meals}
+                  </div>
+
                   <Modal days={day} />
                 </div>
               </Paper>
